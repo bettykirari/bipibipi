bb1d21a5fa74e8158cd9bd12233a0d9217b93c1e
bettykirari <62133402+bettykirari@users.noreply.github.com>
2020-03-27 16:22:32 +0800

    Add files via upload

---
+++ plurk/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlurkConfig(AppConfig):
+    name = 'plurk'
---
+++ views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def sayhello(request):
+    return HttpResponse("Hello Django!")# Create your views here.
